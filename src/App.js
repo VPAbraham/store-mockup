aae4c2bfe890dad3d889582b1583dee01936ec4a
Victor Abraham <vpa4565@gmail.com>
2021-01-27 10:27:32 -0700

    Set up routing and props management withRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.styles.scss';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import HatsPage from './pages/hatspage/hatspage.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/hatspage' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/pages/hatspage/hatspage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './hatspage.styles.scss';
+
+const HatsPage = () => {
+  return (
+    <div className='hatspage'>
+      <p>Hello this is the hats page.</p>
+    </div>
+  )
+}
+
+export default HatsPage;
