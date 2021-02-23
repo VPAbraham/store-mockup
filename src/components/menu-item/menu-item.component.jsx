01bd69cf71806691dc7ad400f92c0d1fbe10a612
VPAbraham <vpa4565@gmail.com>
2021-02-22 19:56:40 -0700

    Small change

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './menu-item.styles.scss';
 import { withRouter } from 'react-router-dom'
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match}) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`}
+     onclick={() => history.push(`/${match.url}${linkUrl}`)} 
+     >
       <div 
       className='background-image' 
       style={{
@@ -19,4 +21,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
   )
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
